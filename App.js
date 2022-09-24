3a07ed49f1c344cd8a1078cf17625d2f119a6752
erik <erik_g@web.de>
2022-09-24 20:07:29 +0300

    Assignment8

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, TextInput, Dimensions } from 'react-native';
+import { useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+
+  const key = 'k1tDgHbYqlAa4QzK1TOMJG6nNxD0mmCp';
+
+  const [address, setAddress] = useState('');
+
+  const [data, setData] = useState('');
+
+  const [position, setPosition] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302
+  });
+
+  const fetchRep = () => {
+
+    fetch('http://www.mapquestapi.com/geocoding/v1/address?key=' + key + '&location=' + address)
+      .then(response => response.json())
+      .then(data => setData((((data.results)[0].locations)[0].latLng)))
+      .catch(err => Alert.alert('Error', err));
+
+    setPosition({
+      latitude: data.lat,
+      longitude: data.lng
+    });
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <MapView
+        style={styles.map}
+        region={{
+          ...position,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        }}>
+        <Marker
+          coordinate={{ ...position }}
+          title={address}
+        />
+      </MapView>
+
+      <View style={{ flex: 1 }}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Type in your address'
+          onChangeText={(address) => setAddress(address)}
+        />
+        <View style={styles.buttonView}>
+          <Button title='SHOW' onPress={fetchRep} />
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -17,4 +67,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    width: Dimensions.get('window').width,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  buttonView: {
+    width: Dimensions.get('window').width,
+  },
+  map: {
+    flex: 6,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
 });
